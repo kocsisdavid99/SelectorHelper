c688fccfad7f31abf4dfa5efd9032c3114d4d20e
kocsisdavid99 <kocsisdavid99@gmail.com>
2019-02-26 01:31:31 +0100

    Add some things

--- test.py
+++ test.py
@@ -5,31 +5,59 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
 
+raw_url_list = open("url_list.txt", "r")
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--silent')
-options.add_argument('--disable-gpu')
-options.add_argument('--log-level=3')
-        
-browser = webdriver.Chrome(executable_path='./chromedriver.exe', chrome_options=options)
+for row in raw_url_list:
+    row = row.split()
+    row = str(row)
+    row = row.strip()
+    
+   
+    row = row.replace("['","")
+    row = row.replace("']","")
+    row = row.replace("', '›","")
+    print(row)
+
+link_selectors = ['.entry-title>a',
+                  'a.post-title',
+                  'a.title',
+                  '.news-title>a',
+                  '.post-title>a']
+
+
 
 first_article_page = []
 
+def request(url, find_element):
 
-browser.get('http://akuaku.pl/blog/')
-source = browser.page_source
-elements = browser.find_elements_by_css_selector('.entry-title>a')
+
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--silent')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--log-level=3')
+
+    browser = webdriver.Chrome(executable_path='./chromedriver.exe', chrome_options=options)
+    browser.get(url)
+    source = browser.page_source
+    elements = browser.find_elements_by_css_selector(find_element)
+    current_url = browser.current_url
+
+    return elements, options, current_url
+
+
+elements, options, current_url = request('http://akuaku.pl/blog/','.entry-title>a')
+
+
 if not elements == []:
     for element in elements:
         element = element.get_attribute('href')
         print(element)
         while len(first_article_page) < 1:
             first_article_page.append(element)
-        
-    browser = webdriver.Chrome(executable_path='./chromedriver.exe', chrome_options=options)
-    browser.get(first_article_page[0])
-    print('A jelenlegi oldal:',browser.current_url)
+
+    elements, options, current_url = request(first_article_page[0],'.entry-title>a')
+    print('A jelenlegi oldal:',current_url)
     print(first_article_page)
         
 else:
