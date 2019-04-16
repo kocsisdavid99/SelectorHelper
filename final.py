32c920d048c4b965eb060ca9bc0378ed90958b79
kocsisdavid99 <kocsisdavid99@gmail.com>
2019-04-17 00:09:14 +0200

    merged from selenium branch

---
+++ final.py
@@ -0,0 +1,131 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+import SelectorStatistics
+from SelectorStatistics import SelectorStatistics
+
+
+
+def get_lines_from_file(filename):
+    file = open(filename, "r")
+    result = []
+
+    for line in file:
+        line = line.replace("\n", "")
+        if line != "":
+            result.append(str(line))
+
+    return result
+
+
+def set_chrome_driver_location():
+    chrome_driver_location = "./chromedriver.exe"
+    return chrome_driver_location
+
+
+def request(url):
+    try:
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--silent')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--log-level=3')
+
+        browser = webdriver.Chrome(executable_path=set_chrome_driver_location(), chrome_options=options)
+        browser.get(url)
+
+        return browser
+
+
+    except Exception:
+        print("Az oldalt nem sikerült betölteni: '" + url + "'")
+
+
+def find_elements(browser, selector):
+    try:
+        element = browser.find_element_by_css_selector(selector)
+
+        return element
+
+    except NoSuchElementException:
+        return None
+
+
+def main():
+
+    LinkSelectorStatistics = SelectorStatistics()
+    MainPageSelectorStatistics = SelectorStatistics()
+    DateSelectorStatistics = SelectorStatistics()
+    AuthorSelectorStatistics = SelectorStatistics()
+
+    for url in get_lines_from_file("url_list.txt"):
+        # print("Asking url: " + url)
+        browser = request(url)
+        if browser:
+
+            result_line = url
+            for link_selector in get_lines_from_file("link_selector_list.txt"):
+                # print("   Checking link selector: " + link_selector)
+                link_element = find_elements(browser, link_selector)
+                if (link_element):
+
+                    result_line += " |Link selector: " + link_selector
+                    LinkSelectorStatistics.increment(link_selector)
+
+                    mainPageLink = link_element.get_attribute('href')
+
+                    mainPageBrowser = request(mainPageLink)
+
+                    if mainPageBrowser:
+
+                        for mainPageSelector in get_lines_from_file("main_page_selector_list.txt"):
+                            # print("      Checking main page selector: " + mainPageSelector)
+                            main_page_element = find_elements(mainPageBrowser, mainPageSelector)
+
+                            if (main_page_element):
+                                result_line += " |Main page selector: " + mainPageSelector
+                                MainPageSelectorStatistics.increment(mainPageSelector)
+
+                                break
+
+                        for dateSelector in get_lines_from_file("date_selector_list.txt"):
+                            date_element = find_elements(mainPageBrowser, dateSelector)
+
+                            if (date_element):
+                                result_line += " |Date selector: " + dateSelector
+                                DateSelectorStatistics.increment(dateSelector)
+
+                                break
+
+                        for authorSelector in get_lines_from_file("author_selector_list.txt"):
+                            author_element = find_elements(mainPageBrowser, authorSelector)
+
+                            if (author_element):
+                                result_line += " |Author selector: " + authorSelector
+                                AuthorSelectorStatistics.increment(authorSelector)
+
+                                break
+
+
+
+                    break
+
+        print(result_line)
+
+    print("Link selector statistics:")
+    LinkSelectorStatistics.print()
+
+    print("Main page selector statistics:")
+    MainPageSelectorStatistics.print()
+
+    print("Date selector statistics:")
+    DateSelectorStatistics.print()
+
+    print("Author selector statistics:")
+    AuthorSelectorStatistics.print()
+
+
+
+
+if __name__ == '__main__':
+    main()
